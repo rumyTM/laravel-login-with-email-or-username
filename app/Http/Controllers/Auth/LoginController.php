73fcb10aeaedce78c7a265db037f08bf0d4d9230
rumyTM <rumywithy@gmail.com>
2021-09-12 03:10:34 +0600

    Implemented logics in Login & Register Controllers

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -36,5 +37,18 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    protected function attemptLogin(Request $request): bool
+    {
+        $fieldName = filter_var($request->email, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+
+        return $this->guard()->attempt(
+            [
+                $fieldName => $request->email,
+                'password' => $request->password
+            ],
+            $request->filled('remember')
+        );
     }
 }
